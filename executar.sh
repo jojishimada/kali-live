d39196a20edb7b2873156ca2880a6d971becd326
jojishimada <jojishimada@gmail.com>
2022-07-03 13:29:31 +0000

    nova3

--- executar.sh
+++ executar.sh
@@ -3,6 +3,7 @@ apt update
 apt install openssh-server
 systemctl start ssh.socket
 wget https://github.com/zaproxy/zaproxy/releases/download/v2.11.1/ZAP_2_11_1_unix.sh
+chmod 777 ZAP_2_11_1_unix.sh 
 ./ZAP_2_11_1_unix.sh 
 owasp
 msfdb init
